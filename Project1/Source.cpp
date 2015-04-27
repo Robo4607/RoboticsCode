e91184a62205c278c63232672d92031bf28dd568
CIS 4607 <beckerrobo@gmail.com>
2015-04-27 15:59:49 -0500

    Visual Studio Source Control

---
+++ Project1/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Gooch";
+
+	cout << "\n\n";
+	
+	 string gooch;
+
+	getline (cin, gooch);
+
+	if (gooch == "Perrin") {
+		cout << "\n";
+		cout << "Pudding Pop";
+		cout << "\n\n";
+	}
+
+	else if (gooch != "Perrin") {
+			cout << "\n";
+			cout << "Pneumatics";
+			cout << "\n\n";
+	}
+
+	if (gooch == "69") {
+		cout << "Goochmobile";
+		cout << "\n\n";
+	}
+
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
